1e47e0ee6bcc9a831fc08d1335727008ce86e871
DanielKimster <70424200+DanielKimster@users.noreply.github.com>
2021-06-05 17:35:31 -1000

    Work-in-progress
    added schemas for adding characters and items

---
+++ app/imports/api/item/Item.js
@@ -0,0 +1,27 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Encapsulates state and variable values for this collection. */
+class ItemsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'Bids';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      item: String,
+      image: String,
+      damage: Number,
+      properties: String,
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    // this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+export const Items = new ItemsCollection();
