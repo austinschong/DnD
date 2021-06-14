e4c87c20e884dfb4711f1ded7b82029a7e39a3a5
DanielKimster <70424200+DanielKimster@users.noreply.github.com>
2021-06-13 20:04:45 -1000

    Cleanup of Stuffs collection

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,28 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
-import { Stuffs } from '../../api/stuff/Stuff';
 import { Characters } from '../../api/character/Character';
 import { ImageData } from '../../api/image/ImageData';
 import { Items } from '../../api/item/Item';
-
-// User-level publication.
-// If logged in, then publish documents owned by this user. Otherwise publish nothing.
-Meteor.publish(Stuffs.userPublicationName, function () {
-  if (this.userId) {
-    const username = Meteor.users.findOne(this.userId).username;
-    return Stuffs.collection.find({ owner: username });
-  }
-  return this.ready();
-});
-
-// Admin-level publication.
-// If logged in and with admin role, then publish all documents from all users. Otherwise publish nothing.
-Meteor.publish(Stuffs.adminPublicationName, function () {
-  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
-    return Stuffs.collection.find();
-  }
-  return this.ready();
-});
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise publish nothing.
